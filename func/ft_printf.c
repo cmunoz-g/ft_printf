44b581d4bd86f7a350d6d081a800d2c5adc8f0e5
Carlos <carlosmg.mkt@gmail.com>
2023-12-28 12:34:45 +0100

    c

--- func/ft_printf.c
+++ func/ft_printf.c
@@ -1,21 +1,21 @@
 #include "ft_printf.h"
 
-void	ft_printf_aux(va_list *args, const char type, int *count)
+void	ft_printf_aux(va_list args, const char type, int *count)
 {
 	if (type == 'c')
-		ft_putchar(va_arg(*args, int), count);
+		ft_putchar(va_arg(args, int), count);
 	else if (type == 's')
-		ft_putstr(va_arg(*args, char *), count);
+		ft_putstr(va_arg(args, char *), count);
 	else if (type == 'p')
-		ft_putptr(va_arg(*args, void *), count);
+		ft_putptr(va_arg(args, void *), count);
 	else if (type == 'd' || type == 'i')
-		ft_putnbr(va_arg(*args, int), count);
+		ft_putnbr(va_arg(args, int), count);
 	else if (type == 'u')
-		ft_putunbr(va_arg(*args, unsigned int), count);
+		ft_putunbr(va_arg(args, unsigned int), count);
 	else if (type == 'x')
-		ft_puthex(va_arg(*args, unsigned int), "0123456789abcdef", count);
+		ft_puthex(va_arg(args, unsigned int), "0123456789abcdef", count);
 	else if (type == 'X')
-		ft_puthex(va_arg(*args, unsigned int), "0123456789ABCDEF", count);
+		ft_puthex(va_arg(args, unsigned int), "0123456789ABCDEF", count);
 	else if (type == '%')
 		ft_putchar('%', count);
 }
@@ -28,7 +28,9 @@ int	ft_printf(const char *str, ...)
 
 	va_start(args, str);
 	count = 0;
-	ch = va_arg(args, const char *);
+	if (str == NULL)
+		return (-1);
+	ch = str;
 	while (*ch != '\0')
 	{
 		if (*ch == '%')
@@ -36,7 +38,7 @@ int	ft_printf(const char *str, ...)
 			ch++;
 			count++;
 			if (*ch != '\0')
-				ft_printf_aux(&args, *ch, &count);
+				ft_printf_aux(args, *ch, &count);
 		}
 		else
 		{
